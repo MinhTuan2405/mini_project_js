7498beb314d8be0a27a06a7b295b7cf0c2f8eb02
mnhtndev <91857446+MinhTuan2405@users.noreply.github.com>
2024-09-20 19:19:41 +0700

    refactor main.js

--- main.js
+++ main.js
@@ -1,111 +1,119 @@
-//Get elements to make logic for the game
-
-// the secret number: to display on screen
-const secret = document.querySelector ('.number')
-
-// the play button again: when the player wants to player again or reset the game
-const playAgainButton = document.querySelector ('.again')
-
-// the checking result button: send the guess number to process
-const checkButton = document.querySelector ('.check')
-
-// the guess number: contain the guess number
-const guessCell = document.querySelector ('.guess')
-
-// the current score
-const currentScore = document.querySelector ('.score')
-
-// the current high score
-const highScore = document.querySelector ('.highscore')
-
-// the message when playing the game, including (winning game, guessing lower, higher, and losing game)
-const messages = document.querySelector ('.message')
-
-// the background of game
-const backgroud = document.querySelector ('body')
-
-// list constant of game
-const NOTHING = 0
-
-// random a secret key 
-// Input: no arguments
-// Output: a secret key
-const randomNewSecret = () => {
-    return Math.trunc (Math.random () * 20) + 1
-}
-
-// Create an initial secret
-let secretNumber = randomNewSecret ()
-
 // Display the message on the screen
 // Input: message: String
 //        color: string, express the hexa code of color
 // Output: None
-const displayMessage = (message, color) => {
+// Process: display the message on screen (on the message cell) and set the game's backgroud with given color
+
+const displayMessageAndBackgroud = (message, color) => {
+    
+    // the message when playing the game, including (winning game, guessing lower, higher, and losing game)
+    const messages = document.querySelector ('.message')
     messages.innerHTML = message
+
+    // the background of game
+    const backgroud = document.querySelector ('body')
     backgroud.style.backgroundColor = color
+}
+
+// random a secret key 
+// Input: no arguments
+// Output: a secret key
+// Process: return the secret number through the random method in Math object
+
+const randomNewSecretNumber = () => {
+    return Math.trunc (Math.random () * 20) + 1
 }
 
 // Reset the game, ignore the high score
 // Input: none
 // Output: None
-const reset = () => {
-    // reset all the element of game, ignore the high score information
-    displayMessage ('Start guessing...', 'black')
-    secretNumber = randomNewSecret ()
-    console.log (secretNumber)
+// Process: reset all the element of game, ignore the high score information
+
+const resetGame = () => {
+   // display the starting game screen
+    displayMessageAndBackgroud ('Start guessing...', 'black')
+
+    // create new secret number for the next round
+    secretNumber = randomNewSecretNumber ()
+
+    // reset the score 
     currentScore.innerHTML = 20
     score = 20
+
+    // hide the secret number and input cell
     secret.innerHTML = '?'
     guess.innerHTML = ''
 }
 
+// define some parameter for starting first game
 // set first high score
 let highestScore = 0
+
 //Set current score for every new game
 let score = 20
 
+// Create an initial secret
+let secretNumber = randomNewSecretNumber ()
+
+// nothing happen status
+const NOTHING = 0
+
+// minimum score player can reach
+const minimum = 1
+
+// the secret number: to display on screen
+const secret = document.querySelector ('.secretnumber')
+
 // handle the click event when the player clicks the checkButton
-checkButton.addEventListener ('click', () => {
+document.querySelector ('.check').addEventListener ('click', () => {
+    
     // get the input number
-    const guessNumber = Number (guessCell.value)
+    const guessNumber = Number (document.querySelector ('.guess').value)
 
     // process if player any enter anything
     if (guessNumber === NOTHING) {
-        displayMessage ('you did not type any number', 'gray')
-    }else {
-        // process if the player wins the game
-        if (guessNumber === secretNumber) {
-            displayMessage ('u r win the game 🎉🎉🎉', 'green')
-            secret.innerHTML = secretNumber
-            //Update the high score
-            if (highestScore < score) {
-                highScore.innerHTML = score
-            }
-        // process if the player guesses an incorrect answer 
+        displayMessageAndBackgroud ('you did not type any number', 'gray')
+    }
+        
+    // process if the player wins the game
+    if (guessNumber === secretNumber) {
+        // display the winning notification 
+        displayMessageAndBackgroud ('u r win the game 🎉🎉🎉', 'green')
+
+        // make the secret number visiable 
+        secret.innerHTML = secretNumber
+        
+        //Update the highest score
+        if (highestScore < score) {
+            document.querySelector ('.highscore').innerHTML = score
         } else {
-            if (guessNumber > secretNumber) {
-                displayMessage ('too high', 'orange')
-                --score
-                currentScore.innerHTML = score
-            }else {
-                displayMessage ('too low', 'blue')
-                --score
-                currentScore.innerHTML = score
-            }
+            highestScore = score
         }
     }
-    // process if the player loses the game
-    if (score < 1) {
-        displayMessage(`you loose game, secret is ${secretNumber}`, 'gray')
-        secret.innerHTML = secretNumber
-        // reset game and start a new roud
-        setTimeout(reset, 2500);
-    }
     
+    // process if the player give an incorrect answer
+    // the current score
+    const currentScore = document.querySelector ('.score')
+    if (guessNumber > secretNumber) {
+        displayMessageAndBackgroud ('too high', 'orange')
+        currentScore.innerHTML = score--
+    }else {
+        displayMessageAndBackgroud ('too low', 'blue')
+        currentScore.innerHTML = score--
+        }
+    }
+                                                    
+    // process if the player loses the game 
+    if (score < minimum) {
+        // display if had lost the game
+        displayMessageAndBackgroud(`you loose game, secret is ${secretNumber}`, 'gray')
+        secret.innerHTML = secretNumber
+    
+        // reset game and start a new roud after 2.5s
+        setTimeout(resetGame, 2500);
+    }
 })
 
 // handle the click event for the playButton when player wanna play again or cancel the current roud
-playAgainButton.addEventListener ('click', () => {
-    reset ()
-})
+document.querySelector ('.again').addEventListener ('click', resetGame ())
+
